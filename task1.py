47a080529f10e33fcaa9b2f748bc800833672d6c
Korey Liu <koreyliu1221@gmail.com>
2023-07-06 20:33:17 -0400

    Moved some helper functions into utils.py \n task1.py reads 100 validated clips and calculates word error rate and cos along with the average

--- task1.py
+++ task1.py
@@ -1,40 +1,13 @@
-from api import whisper_query
+from api import whisper_query, sentence_transformer
+import utils
 import csv
 import os
 
 
-def get_validated_data() -> dict:
-    """
-    Creates a dictionary that maps the validated file name to the data \n
-    {[audio_file_name] -> {"client_id", "path", "sentence", "up_votes", "down_votes", "age", "gender", "accents", "variant", "locale", "segment"}}
-    """
 
-    validated_dict = {}
+def init_output_arr(headers: list):
 
-    with open("./dataset/validated.tsv", 'r', encoding='utf8') as validated_file:
-        
-        validated_data = csv.reader(validated_file, delimiter='\t')
-
-        header = next(validated_data)
-        
-        for data in validated_data:
-
-            data_item = {}
-
-            for i in range(len(header)):
-                header_name = header[i]
-                data_item[header_name] = str(data[i])
-            
-            file_name = data_item['path']
-
-            validated_dict[file_name] = data_item
-
-    return validated_dict
-
-
-def init_output_arr():
-
-    output_headings = ["Whisper Transcription", "Expected Transcription", "Word Error Rate"]
+    output_headings = headers
 
     heading = "\t".join(output_headings)
     heading += "\n"
@@ -42,21 +15,23 @@ def init_output_arr():
     return [heading]
 
 
+
 ## task1
-N_CLIPS = 10
+N_CLIPS = 100
+wh_model_type = "medium"
 
-output_arr = init_output_arr()
-
-whisper = whisper_query.setup_whisper("tiny")
-
-validated_files = get_validated_data()
+output_arr = init_output_arr(["whisper_transcription", "expected_transcription", "word_error_rate", "cosine_similarity"])
+whisper = whisper_query.setup_whisper(wh_model_type)
+validated_files = utils.get_validated_data()
+wer = utils.wer_calculator()
+sentence_model = sentence_transformer.get_sentence_model()
 
 data_path = "./dataset/clips/"
 with os.scandir(data_path) as audio_files:
     total_clips = 0
     for mp3file in audio_files:
 
-        if total_clips > N_CLIPS:
+        if total_clips >= N_CLIPS:
             break
         
         audio_file = mp3file.name
@@ -68,11 +43,14 @@ with os.scandir(data_path) as audio_files:
 
             whisper_transcription = whisper(file_name)
 
-            transcription_text = whisper_transcription['text'].strip()
-            expected_text = validated_files[audio_file]['sentence'].strip()
+            transcription_text = str(whisper_transcription['text'].strip())
+            expected_text = str(validated_files[audio_file]['sentence'].strip())
+
+            word_error_rate = wer.compute(predictions=[transcription_text], references=[expected_text])
+            cos_sim = utils.cos_sim_calc(sentence_model, transcription_text, expected_text).item()
 
 
-            output_data = "\t".join([transcription_text,expected_text]) + "\n"
+            output_data = "\t".join([transcription_text,expected_text, str(word_error_rate), str(cos_sim)]) + "\n"
             output_arr.append(output_data)
             
 
@@ -81,3 +59,25 @@ with os.scandir(data_path) as audio_files:
 
 with open("outputs/task1_output.txt", 'w', encoding='utf-8') as output:
     output.writelines(output_arr)
+
+
+# Find Avg WER and Cosine Similarity
+with open("outputs/task1_output.txt", 'r', encoding='utf-8') as input_file:
+
+    data = csv.reader(input_file, delimiter="\t")
+
+    headers = next(data)
+    wer = headers.index("word_error_rate")
+    cos_sim = headers.index("cosine_similarity")
+
+    total_wer = 0
+    total_cos_sim = 0
+
+    count = 0
+    for line in data:
+        count += 1
+        total_wer += float(line[wer])
+        total_cos_sim += float(line[cos_sim])
+    
+    print(f"DATA FROM {N_CLIPS} CLIPS using whisper-{wh_model_type} \n\tAVG WORD ERROR RATE: {total_wer/count} \n\tAVG COSINE SIMILARITY: {total_cos_sim/count}")
+
---
+++ utils.py
@@ -0,0 +1,49 @@
+import evaluate
+from sentence_transformers import SentenceTransformer, util
+
+def get_validated_data() -> dict:
+    """
+    Creates a dictionary that maps the validated file name to the data \n
+    {[audio_file_name] -> {"client_id", "path", "sentence", "up_votes", "down_votes", "age", "gender", "accents", "variant", "locale", "segment"}}
+    """
+
+    validated_dict = {}
+
+    with open("./dataset/validated.tsv", 'r', encoding='utf8') as validated_file:
+        
+        validated_data = csv.reader(validated_file, delimiter='\t')
+
+        header = next(validated_data)
+        
+        for data in validated_data:
+
+            data_item = {}
+
+            for i in range(len(header)):
+                header_name = header[i]
+                data_item[header_name] = str(data[i])
+            
+            file_name = data_item['path']
+
+            validated_dict[file_name] = data_item
+
+    return validated_dict
+
+def wer_calculator():
+    """
+    returns calculator to calculate word error rate (from evaluate module). call `.compute`
+    """
+    calc = evaluate.load("wer")
+    return calc
+
+def cos_sim_calc(sentence_model: SentenceTransformer, prediction_sentence: str, reference_sentence: str) -> float:
+    # taken from https://huggingface.co/tasks/sentence-similarity; calculates sentence semantic
+    '''
+    calculates cos_sim of two sentences
+    '''
+    prediction_embedding = sentence_model.encode(prediction_sentence)
+    reference_embedding = sentence_model.encode(reference_sentence)
+
+    cosine_similarity =  util.pytorch_cos_sim(prediction_embedding,reference_embedding)
+
+    return cosine_similarity
